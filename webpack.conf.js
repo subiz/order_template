b14bcf06346d68fbedac6ff86e732250aad15618
Thanh Pham Kieu <thanhpk@live.com>
2022-01-18 11:27:44 +0700

    fix build

--- webpack.conf.js
+++ webpack.conf.js
@@ -3,9 +3,10 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
 	mode: 'production',
 	entry: {index: './index.js'},
-	output: {publicPath: './'}, // so we can access html locally
+	output: {publicPath: process.env.ENV === 'dev' ? '/' : './'}, // so we can access html locally
 	devServer: {port: 8089, host: '0.0.0.0', hot: false, liveReload: false},
 	optimization: {minimize: false},
+	performance: {hints: false},
 	plugins: [
 		new HtmlWebpackPlugin({filename: './index.html', template: './index.html', inject: 'body', chunks: ['index']}),
 	],
