c41c9fa46782ec34c942e2c07026e12c7feb2832
Thanh Pham Kieu <thanhpk@live.com>
2022-01-18 11:26:13 +0700

    fix tax and discount

--- src/template.js
+++ src/template.js
@@ -2,15 +2,19 @@ import {h, Component} from 'preact'
 import * as util from './util.js'
 
 let gstyle = `
-.invoice_sublabel {
-	width: 100px;
+.text__muted {
+	color: #888;
 }
 
-.order_print_label {
-	font-weight: 600;
+.mt-1 {
+margin-top:10px
+}
+
+.invoice_sublabel {
+	font-weight: 400;
 	text-transform: uppercase;
 	font-size: 13px;
-	color: #333;
+	color: #666;
 }
 
 .table.order_print_table {
@@ -71,7 +75,7 @@ export default class Main extends Component {
 		return (
 			<div style='margin-top: 0.5cm'>
 				<div style='display: flex; align-items: center; margin-top: 20px'>
-					<div class='order_print_label'>{this.props.t('payment')}</div>
+					<div class='invoice_sublabel'>{this.props.t('payment')}</div>
 				</div>
 				<div>COD</div>
 			</div>
@@ -84,11 +88,9 @@ export default class Main extends Component {
 		return (
 			<div style='margin-top: 30px'>
 				<div class='d-flex' style='align-items: center; margin-top: 20px'>
-					<div class='order_print_label'>{this.props.t('terms_and_conditions')}</div>
+					<div class='invoice_sublabel'>{this.props.t('terms_and_conditions')}</div>
 				</div>
-				<div class='text__muted'>
-					{this.props.t.i18n(template.i18n_terms_and_conditions, template.terms_and_conditions)}
-				</div>
+				<div>{this.props.t.i18n(template.i18n_terms_and_conditions, template.terms_and_conditions)}</div>
 			</div>
 		)
 	}
@@ -166,18 +168,13 @@ export default class Main extends Component {
 		let adjustment = order.adjustment || 0
 		if (adjustment != 0 || order.adjustment_description)
 			$adjustment = (
-				<div>
-					<div style='margin-top: 5px' title={this.props.t('adjustment_desc')}>
-						{this.props.t('adjustment')}
+				<div style='align-items: center; display: flex; justify-content: space-between;'>
+					<div style='width: 140px'>
+						{order.adjustment_description || <span style='color: #888'>{this.props.t('empty')}</span>}
 					</div>
-					<div style='align-items: center; display: flex; margin-top: 2px'>
-						<div style='width: 140px'>
-							{order.adjustment_description || <span style='color: #888'>{this.props.t('empty')}</span>}
-						</div>
-						<div title={this.props.t('adjustment_desc')}></div>
-						<div style='flex: 1' style='text-align: right'>
-							{util.formatNumber(adjustment)}
-						</div>
+					<div title={this.props.t('adjustment_desc')}></div>
+					<div style='flex: 1' style='text-align: right'>
+						{util.formatNumber(adjustment)}
 					</div>
 				</div>
 			)
@@ -196,15 +193,15 @@ export default class Main extends Component {
 		let total = order.total
 		let subtotal = order.subtotal
 
-		let discounttype = order.discount_type || 'percentage'
-		discounttype += order.discount_before_tax ? '_before_tax' : '_after_tax'
+		let discountbeforetax = items
+			.map((item) => {
+				let discounttype = item.discount_type || 'percentage'
 
-		let discount = 0
-		if (discounttype.startsWith('percentage')) {
-			discount = (order.discount_percentage || 0) / 100
-		} else if (discounttype.startsWith('amount')) {
-			discount = order.discount
-		}
+				let price = (item.product && item.product.price) || 0
+				let item_total = price * item.quantity
+				return item_total - item.total
+			})
+			.reduce((s, a) => s + a, 0)
 
 		let $note = <div style='flex: 1; padding: 0' />
 		if (order.note) {
@@ -217,10 +214,37 @@ export default class Main extends Component {
 		}
 		let left = (order.total || 0) - (order.payment_made || 0)
 		if (left <= 0) left = 0
+
+		let $discountbeforetax = null
+		if (discountbeforetax) {
+			$discountbeforetax = (
+				<div style='display: flex; justify-content: space-between; align-items: center'>
+					<div style='flex-shrink: 0'>{this.props.t('discount_before_tax')}&nbsp;</div>
+					<div style={`font-family: ${template.number_font_family}`}>-{util.formatNumber(discountbeforetax)}</div>
+				</div>
+			)
+		}
+
+		let discounttype = order.discount_type || 'percentage'
+		let discount = 0
+		if (discounttype == 'percentage') {
+			discount = '-'+(order.discount_percentage || 0) / 100 + '%'
+		} else {
+			discount = '-'+util.formatNumber(order.discount_amount)
+		}
+		let $discountaftertax = null
+		if (discount) {
+			$discountaftertax = (
+				<div style='display: flex; justify-content: space-between; align-items: center'>
+					<div style='flex-shrink: 0'>{this.props.t('discount_after_tax')}&nbsp;</div>
+					<div style={`font-family: ${template.number_font_family}`}>{discount}</div>
+				</div>
+			)
+		}
 		return (
 			<div style='margin-top: 30px; padding: 0px 2cm;'>
 				<div style='display: flex;align-items: center; margin-top: 20px; margin-bottom: 5px'>
-					<div class='order_print_label'>{}</div>
+					<div class='invoice_sublabel'>{}</div>
 					<div style='flex: 1' />
 					<div>
 						{this.props.t('currency')}: {order.currency || acc.currency || 'VND'}
@@ -236,21 +260,21 @@ export default class Main extends Component {
 				<div class='page_section' style='display: flex; margin-top: 10px'>
 					{$note}
 					<div style='width: 6cm; margin-left: 10px'>
+						{$discountbeforetax}
 						<div style='display: flex; justify-content: space-between;'>
 							<div>{this.props.t('subtotal')}</div>
 							<div style={`font-family: ${template.number_font_family}`}>{util.formatNumber(subtotal)}</div>
 						</div>
-
-						<div style='display: flex; justify-content: space-between; align-items: center'>
-							<div style='flex-shrink: 0'>{this.props.t('discount')}&nbsp;</div>
-							<div style={`font-family: ${template.number_font_family}`}>{discount}</div>
-						</div>
 						{$ship}
-						{this.renderOrderTax()}
+						<div style='display: flex; justify-content: space-between;'>
+							<div>{this.props.t('tax')}</div>
+							<div style={`font-family: ${template.number_font_family}`}>{util.formatNumber(order.total_tax)}</div>
+						</div>
+						{$discountaftertax}
 						{$adjustment}
-						<div style='display: flex; justify-content: space-between;align-items: center; margin-top: 5px; margin-bottom: 10px'>
-							<b>{this.props.t('total')}</b>
-							<div style={`font-weight: 600;font-family: ${template.number_font_family}`}>
+						<div style='display: flex; justify-content: space-between;align-items: center; margin-top: 15px;'>
+							<b style='font-size: 18px'>{this.props.t('total')}</b>
+							<div style={`font-size: 18px; font-weight: 600;font-family: ${template.number_font_family}`}>
 								{util.formatNumber(total)}
 							</div>
 						</div>
@@ -260,45 +284,12 @@ export default class Main extends Component {
 						</div>
 						<div style='display: flex; justify-content: space-between; '>
 							<div>{this.props.t('balance_due')}</div>
-							<div style={`font-family: ${template.number_font_family}`}>{util.formatNumber(left)}</div>
+							<div style={`font-family: ${template.number_font_family}; color: ${template.primary_color}`}>
+								{util.formatNumber(left)}
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		)
-	}
-
-	renderOrderTax = () => {
-		let taxDict = {}
-		let priceM = {}
-		let order = this.props.order
-		let template = this.props.template
-
-		let totaltax = 0
-		let computed_discount = order._computed_discount
-		let taxM = order._taxM
-		let subtotal = order.subtotal
-		if (subtotal == 0) subtotal = 0.001
-
-		Object.values(taxM || {}).map((t) => {
-			taxDict[t.tax.id] = t.tax
-			priceM[t.tax.id] = priceM[t.tax.id] || 0
-			priceM[t.tax.id] += t.taxprice
-			totaltax += t.taxprice
-		})
-
-		if (Object.keys(priceM).length === 0)
-			return (
-				<div style='display: flex; justify-content: space-between;'>
-					<div>{this.props.t('tax')}</div>
-					<div style={`font-family: ${template.number_font_family}`}>0</div>
-				</div>
-			)
-
-		return (
-			<div style='display: flex; justify-content: space-between;'>
-				<div>{this.props.t('tax')}</div>
-				<div style={`font-family: ${template.number_font_family}`}>{util.formatNumber(totaltax)}</div>
 			</div>
 		)
 	}
@@ -314,7 +305,7 @@ export default class Main extends Component {
 		if (address) {
 			$shipping = (
 				<div style='flex: 1;'>
-					<div class='order_print_label'>{this.props.t('shipping_to')}</div>
+					<div class='invoice_sublabel mt-1'>{this.props.t('shipping_to')}</div>
 					<div>{address}</div>
 				</div>
 			)
@@ -339,7 +330,7 @@ export default class Main extends Component {
 				<div style='display: flex; flex-direction: column; justify-content: flex-end'>
 					<div style='flex: 1' />
 
-					<div class='order_print_label' style='margin-top: 10px'>
+					<div class='invoice_sublabel' style='margin-top: 10px'>
 						{this.props.t('customer')}
 					</div>
 					<div>{userdisplayname}</div>
@@ -348,33 +339,19 @@ export default class Main extends Component {
 					{$shipping}
 				</div>
 				<div style='flex: 1' />
-				<div style='display: flex; flex-direction: column; justify-content: flex-end; text-align: right'>
-					<div style='display: flex;'>
-						<div class='invoice_sublabel'>{this.props.t('order_number')}</div>
-						&nbsp;
-						<div style='width: 100px'>{order.number || order.id}</div>
-					</div>
-					<div style='display: flex;'>
+				<div style='display: flex; flex-direction: column; text-align: right'>
+					<div>
 						<div class='invoice_sublabel'>{this.props.t('invoice_date')}</div>
-						&nbsp;
-						<div style='width: 100px'>
-							{util.formatDate(order.invoice_date || order.created || Date.now(), 'yyyy/MM/dd')}
-						</div>
+						<div>{util.formatDate(order.invoice_date || order.created || Date.now(), 'yyyy/MM/dd')}</div>
 					</div>
 
-					<div style='display:flex'>
-						<div class='invoice_sublabel'>{this.props.t('due_date')}</div>&nbsp;
-						<div style='width: 100px'>
+					<div>
+						<div class='invoice_sublabel mt-1'>{this.props.t('due_date')}</div>
+						<div>
 							{util.formatDate(order.due_date || order.invoice_date || order.created || Date.now(), 'yyyy/MM/dd')}
 						</div>
 					</div>
-					<div style='display: flex'>
-						<div class='invoice_sublabel'>{this.props.t('balance_due')}</div>
-						&nbsp;
-						<div style={`width: 100px; color: ${template.primary_color}; font-weight: 600`}>
-							{util.formatNumber(left)}
-						</div>
-					</div>
+					<div style='flex: 1' />
 				</div>
 			</div>
 		)
@@ -407,11 +384,12 @@ export default class Main extends Component {
 					</div>
 				</div>
 
-				<div style='display: flex; flex-direction: column; justify-content: end'>
+				<div style='display: flex; flex-direction: column; justify-content: flex-end; align-items: flex-end'>
 					<div
 						style={`text-transform: uppercase; font-size: 32px;  font-weight: 600;  color: ${template.primary_color}; padding: 0px`}>
 						{this.props.t('invoice')}
 					</div>
+					<div class='text__muted'>#{order.number || order.id}</div>
 				</div>
 			</div>
 		)
